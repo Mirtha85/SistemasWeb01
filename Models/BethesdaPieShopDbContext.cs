bb6dd58cd1b8492301fcf87394e48c8242bdc09f
Mirtha Jimenez <mirtha.jimenez.leon@gmail.com>
2023-05-30 20:26:20 -0400

    Added shopping cart item migration and model

--- Models/BethesdaPieShopDbContext.cs
+++ Models/BethesdaPieShopDbContext.cs
@@ -5,11 +5,12 @@ namespace SistemasWeb01.Models
     {
         public BethesdaPieShopDbContext(DbContextOptions<BethesdaPieShopDbContext> options) : base(options)
         {
-            Database.Migrate();// this will migrate the database on startup
-
+            // migrations history is stored at table __EFMigrationsHistory
+            Database.Migrate();// this will apply all the new migrations to the database
         }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Pie> Pies { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
