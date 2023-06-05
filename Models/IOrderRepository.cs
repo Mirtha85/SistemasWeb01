638d9186a9bcc2b3f734cfb82c81e55a59c465d0
Mirtha Jimenez <mirtha.jimenez.leon@gmail.com>
2023-06-04 22:41:48 -0400

    added order models and its migrations

---
+++ Models/IOrderRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace SistemasWeb01.Models
+{
+    public interface IOrderRepository
+    {
+        void CreateOrder(Order order);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
 
 builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
 builder.Services.AddSession();
